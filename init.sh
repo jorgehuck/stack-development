47c7bc08ca3919fc4dc70b9654f9975fb4fa3d4a
Jorge Huck <jorgehuck@gmail.com>
2023-01-28 18:12:43 -0300

    correciones generales

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Export environment variables
+export MYSQL_PORT_HOST="3306"
+export ADMINER_PORT_HOST="8090"
 export MYSQL_ROOT_PASSWORD_HOST="qwerty"
 export MYSQL_USER_HOST="blackcat"
 export MYSQL_PASSWORD_HOST="OjoConLaClave"
 
 # Run docker-compose
-docker-compose up
+docker-compose up -d
